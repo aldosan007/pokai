0531d0455b3e60e19f350ab282ead3a7a091384f
Aldo Rene Rivas Ojeda <aldorene5411@gmail.com>
2025-10-27 23:53:23 -0300

    Configuración inicial del proyecto y primer modelo

---
+++ pokai/android/app/src/main/kotlin/com/example/pokai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pokai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ pokai/lib/data/models/pokemon_list_item.dart
@@ -0,0 +1,39 @@
+// lib/data/models/pokemon_list_item.dart
+
+class PokemonListItem {
+  final int id;
+  final String name;
+  final String spriteUrl;
+
+  // Constructor principal
+  PokemonListItem({
+    required this.id,
+    required this.name,
+    required this.spriteUrl,
+  });
+
+  // Un "constructor de fábrica" que crea un PokemonListItem
+  // desde el JSON que nos da la lista de la PokeAPI.
+  factory PokemonListItem.fromApi(Map<String, dynamic> json) {
+    
+    // El JSON de la lista nos da "name" y "url"
+    final String name = json['name'];
+    final String url = json['url'];
+
+    // 1. Extraemos el ID de la URL.
+    // La URL es como ".../pokemon/25/"
+    // Quitamos la barra final, dividimos por "/" y tomamos el penúltimo trozo.
+    final id = int.parse(url.split('/')[url.split('/').length - 2]);
+
+    // 2. Construimos la URL del sprite oficial.
+    // Esta es la URL estándar para los sprites "oficiales" (mejores que los pixelados).
+    final spriteUrl = 
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png';
+
+    return PokemonListItem(
+      id: id,
+      name: name,
+      spriteUrl: spriteUrl,
+    );
+  }
+}
